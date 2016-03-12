109dfd07910bb761c84c37e72fce4bd6527b4eb3
João Pedro Renan <jota.renan@gmail.com>
2016-03-12 12:25:57 -0300

    Full of errors

---
+++ 10935_uva.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+main() {
+
+    queue<int> deck;
+
+    int n;
+
+    while (cin >> n) {
+
+        for (int i=n; i>0; i--) {
+            deck.push(i);
+        }
+
+        while (deck.size() > 2) {
+            deck.pop();
+            deck.
+        }
+    }
+
+
+}
