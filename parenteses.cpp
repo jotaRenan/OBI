a5c7b3dab8bf4517b1eca28e47e12376860580ac
João Pedro Renan <jota.renan@gmail.com>
2016-03-12 12:00:20 -0300

    Accepted by URI

--- parenteses.cpp
+++ parenteses.cpp
@@ -1,31 +1,27 @@
-// --  URI 1068
-
 #include <bits/stdc++.h>
 #include <string>
 using namespace std;
 
-string check(string exp) {
+int check(string exp) {
     bool isopen = false;
     int npar = 0;
     for (int i=0; i < exp.length(); i++ ) {
         if (exp[i] == '(') {
             isopen = true;
             npar++;
-        } else {
-        if (exp[i] == ')') {
-            isopen = false;
-            npar++;
         }
+        else {
+            if (exp[i] == ')') {
+                isopen = false;
+                npar++;
+            }
         }
     }
 
-    if (isopen == true) {
-        return "incorrect";
+    if (isopen == false && (npar%2 == 0)){
+        return 1;
     }
-    else if (isopen == false && (npar%2 == 0)){
-        return "correct";
-    }
-    else return "incorrect";
+    else return 0;
 
 
 }
@@ -34,6 +30,6 @@ main() {
  string exp;
 
  while (cin >> exp) {
-    cout << check(exp);
+    cout << (check(exp) ? "correct\n" : "incorrect\n");
  }
 }
