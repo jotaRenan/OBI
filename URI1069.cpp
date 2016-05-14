ab72ca44fe21717ee950ce22c2f9711cb3c0e08c
João Pedro Renan <jota.renan@gmail.com>
2016-05-13 23:16:57 -0300

    Add files via upload

---
+++ URI1069.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+main() {
+
+  int n;  //n entradas
+  cin >> n;
+  stack<char> diamonds;
+  string cadeia;
+
+  int resp; //resposta
+  for (int i=0; i<n; i++) {
+    resp = 0;
+
+    cin >> cadeia;
+    for (int j=0; j<cadeia.length(); j++) {
+      if (cadeia.at(j) == '<') {
+        diamonds.push(cadeia.at(j));
+      }
+      else if (cadeia.at(j) == '>') {
+        if (diamonds.top() == '<') {
+          diamonds.pop();
+          resp++;
+        }
+      }
+    }
+    cout << resp << endl;
+    cadeia.clear();
+    while(!diamonds.empty()) {
+      diamonds.pop();
+    }
+  }
+
+}
